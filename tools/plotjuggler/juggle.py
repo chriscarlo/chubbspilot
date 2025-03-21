49e045a6ebaa8626de56724d1d7e1495d77a6e19
chriscarlo <chris.rimoldi@icloud.com>
2025-03-21 11:09:06 -0700

    ds

--- tools/plotjuggler/juggle.py
+++ tools/plotjuggler/juggle.py
@@ -10,6 +10,10 @@ import requests
 import argparse
 from functools import partial
 
+# Disable SSL verification
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 from openpilot.common.basedir import BASEDIR
 from openpilot.selfdrive.car.fingerprints import MIGRATION
 from openpilot.tools.lib.helpers import save_log
@@ -36,7 +40,7 @@ def install():
   os.mkdir(INSTALL_DIR)
 
   url = os.path.join(RELEASES_URL, m + ".tar.gz")
-  with requests.get(url, stream=True, timeout=10) as r, tempfile.NamedTemporaryFile() as tmp:
+  with requests.get(url, stream=True, timeout=10, verify=False) as r, tempfile.NamedTemporaryFile() as tmp:
     r.raise_for_status()
     with open(tmp.name, 'wb') as tmpf:
       for chunk in r.iter_content(chunk_size=1024 * 1024):
