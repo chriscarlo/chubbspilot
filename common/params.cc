a5fd59f93b84358fb11573db5597e58db70c10a4
chriscarlo <chris.rimoldi@icloud.com>
2025-03-25 11:36:27 -0700

    Update params.cc

--- common/params.cc
+++ common/params.cc
@@ -100,6 +100,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"AthenadPid", PERSISTENT},
     {"AthenadUploadQueue", PERSISTENT},
     {"AthenadRecentlyViewedRoutes", PERSISTENT},
+    {"AzureUploadQueue", PERSISTENT},
     {"BootCount", PERSISTENT},
     {"CalibrationParams", PERSISTENT},
     {"CameraDebugExpGain", CLEAR_ON_MANAGER_START},
